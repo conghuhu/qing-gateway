a40e10b04a8f656a7b56f95f0bd8cf135db7b86d
conghuhu <congguoqinghuhu@gmail.com>
2023-01-06 22:47:30 +0800

    chore: remove static front file

--- qing-gateway/qing-admin/src/main/resources/static/server/menuConfig-5QG5HWGI.mjs
+++
@@ -1,9 +0,0 @@
-import {
-  asideMenuConfig
-} from "./chunk-IME5ISHF.mjs";
-import "./chunk-RKIYV4QL.mjs";
-import "./chunk-QYT7L57C.mjs";
-import "./chunk-NISNRQWT.mjs";
-export {
-  asideMenuConfig
-};
